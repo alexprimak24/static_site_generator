cc02c8cc0f47fe158462d1c3b8c233598cbd7b16
alexprimak24 <aprimak43@gmail.com>
2025-05-20 21:58:02 +0000

    added more .md to make more pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,4 @@
+import os
 from markdown_blocks import markdown_to_blocks, markdown_to_html_node
 
 def extract_title(markdown):
@@ -27,9 +28,32 @@ def generate_page(from_path, template_path, dest_path):
     page_title = extract_title(markdown_content)
 
     updated_template = template.replace("{{ Title }}", page_title).replace("{{ Content }}", html_from_markdown)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
     
     f = open(dest_path, "w")
     f.write(updated_template)
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    
 
+
+
+def copy_contents(copy_to, copy_from):
+    
+    if not os.path.exists(copy_to):
+        os.mkdir(copy_to)
+    
+    for item in os.listdir(copy_from):
+        currentpath = os.path.join(copy_from, item)
+        path_to_move_to = os.path.join(copy_to, item)
+        print(f"! Copied from {currentpath} to {path_to_move_to}")
+        
+        if os.path.isfile(currentpath):
+            shutil.copy(currentpath, path_to_move_to)
+        else:
+            os.mkdir(path_to_move_to)
+            copy_contents(path_to_move_to, currentpath)
