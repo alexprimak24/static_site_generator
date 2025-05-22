7166168107d285b3596fbeadc62a9a60a58c02f3
alexprimak24 <aprimak43@gmail.com>
2025-05-22 10:25:19 +0000

    updated build command

--- src/generate_page.py
+++ src/generate_page.py
@@ -13,39 +13,50 @@ def extract_title(markdown):
     raise ValueError("no title found")
         
 
-
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
-    for filename in os.listdir(dir_path_content):
-        from_path = os.path.join(dir_path_content, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
-        if os.path.isfile(from_path):
-            dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path, basepath)
-        else:
-            generate_pages_recursive(from_path, template_path, dest_path, basepath)
-
-
 def generate_page(from_path, template_path, dest_path, basepath):
-    print(f" * {from_path} {template_path} -> {dest_path}")
-    from_file = open(from_path, "r")
-    markdown_content = from_file.read()
-    from_file.close()
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
+    markdown = open(from_path, "r")
+    markdown_content = markdown.read()
+    markdown.close()
+    
     template_file = open(template_path, "r")
     template = template_file.read()
     template_file.close()
 
-    node = markdown_to_html_node(markdown_content)
-    html = node.to_html()
 
-    title = extract_title(markdown_content)
-    template = template.replace("{{ Title }}", title)
-    template = template.replace("{{ Content }}", html)
-    template = template.replace('href="/', 'href="' + basepath)
-    template = template.replace('src="/', 'src="' + basepath)
+    html_from_markdown = markdown_to_html_node(markdown_content).to_html()
+
+    page_title = extract_title(markdown_content)
+
+    updated_template = template.replace("{{ Title }}", page_title)
+    updated_template = updated_template.replace("{{ Content }}", html_from_markdown)
+    updated_template = updated_template.replace('href="/', f'href="{basepath}')
+    updated_template = updated_template.replace('src="/', f'src="{basepath}')
+
+
+
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
-    to_file = open(dest_path, "w")
-    to_file.write(template)
+    
+    f = open(dest_path, "w")
+    f.write(updated_template)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    
+    for item in os.listdir(dir_path_content):
+        currentpath = os.path.join(dir_path_content, item)
+        path_to_move_to = os.path.join(dest_dir_path, item)
+        print(f"! Copied from {currentpath} to {path_to_move_to}")
+
+        if os.path.isfile(currentpath) and currentpath[-3:] == ".md":
+            generate_page(currentpath,template_path, path_to_move_to[:-3] + ".html", basepath)
+            continue
+        if os.path.isfile(currentpath) and not currentpath[-3:] == ".md":
+            raise Exception("Only markdown files supported")
+
+        else:
+            generate_pages_recursive(currentpath, template_path, path_to_move_to, basepath) 
